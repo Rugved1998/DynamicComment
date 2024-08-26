5f13328b86c786a14b3181867728c10e7c633ed2
Aradhika Ramesh <rugved.bmw@gmail.com>
2024-08-26 14:24:04 +0530

    Logic to display login and logout alogwith Comment Inbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import CommentInput from './components/CommentInput';
+import UserName  from './components/UserName';
+import { Button } from '@mui/material';
 
 function App() {
+  const [user,setUser]=useState();
   return (
     // Delivered Code
 
@@ -24,7 +27,23 @@ function App() {
     //   </header>
     // </div>
     <div className="App">
-      <CommentInput/>
+      {user ? (
+          <>
+          <div className="Panel-login">
+            <UserName/>
+            <Button >Logout</Button>
+          </div>
+            <CommentInput />
+          </>
+        ) : (
+          <>
+          <div className="Panel-logout">
+            <Button >Sign in with Google</Button>
+          </div>
+            <CommentInput/>
+          </>
+        )}
+      
 
     </div>
   );
---
+++ src/components/UserName.tsx
@@ -0,0 +1,13 @@
+//Temporary file created for positioning.
+
+const UserName: React.FC = () => {
+let inline={
+    display:'inline'
+}
+
+    return (
+        <div style={inline}> User Name</div>
+     );
+};
+
+export default UserName;
